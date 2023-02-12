ddba255ba0c8fd186b471dccd239c609276a9d03
Brottweiler <Brottweiler@users.noreply.github.com>
2023-02-12 16:30:43 +0100

    Fix online status

--- script.js
+++ script.js
@@ -10,19 +10,6 @@ window.addEventListener("load", function() {
 			document.getElementById("online-status").innerHTML = data.online ? "<span class=\"online\">Online</span>" : "<span class=\"offline\">Offline</span>"
 			document.getElementById("players-online").innerHTML = data.players.online + "/" + data.players.max
 
-			// check if bukkit_extra exists
-			if (data.hasOwnProperty("bukkit_extra")) {
-				let key = Object.keys(data.bukkit_extra.pgm)[0]
-				document.getElementById("current-map").innerHTML = data.bukkit_extra.pgm[key].map.name
-
-			} else { // parse motd otherwise
-				let map = data.description
-				let startIndex = 3
-				let endIndex = 4
-				map = map.split('»')[1]
-				map = map.substring(startIndex, map.length - endIndex)
-				document.getElementById("current-map").innerHTML = map
-			}
 		})
 		.catch(error => {
 			document.getElementById("online-status").innerHTML = "<span class=\"offline\">Offline</span>"
